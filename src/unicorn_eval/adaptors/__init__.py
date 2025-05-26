47a360e960ad3aee0795765199123ea9f6d182fa
Lena Philipp <75608666+Lena235711@users.noreply.github.com>
2025-05-26 12:04:11 +0200

    Radiology tasks 6, 10, 11 (#24)
    - adaptors for tasks 6, 10 and 11
    - metrics for tasks 6, 10 and 11

--- src/unicorn_eval/adaptors/__init__.py
+++ src/unicorn_eval/adaptors/__init__.py
@@ -26,7 +26,7 @@ from unicorn_eval.adaptors.regression import (
     MultiLayerPerceptronRegressor,
 )
 from unicorn_eval.adaptors.detection import DensityMap
-from unicorn_eval.adaptors.segmentation import SegmentationUpsampling
+from unicorn_eval.adaptors.segmentation import SegmentationUpsampling, SegmentationUpsampling3D
 
 __all__ = [
     "KNN",
@@ -40,4 +40,5 @@ __all__ = [
     "MultiLayerPerceptronRegressor",
     "DensityMap",
     "SegmentationUpsampling",
+    "SegmentationUpsampling3D"
 ]
