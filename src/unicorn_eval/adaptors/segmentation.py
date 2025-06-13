Bug fix for radiology after detection merge + optimisation of spider score calculation  (#46)
* fix residual `task_domain = "radiology"` by switching to `MR` / `CT`
* spider score optimization
* bug fix after detection merge and readjustment of patch size for 16GB CPU setup

src/unicorn_eval/adaptors/segmentation.py
@@ -426,19 +426,23 @@ class Decoder3D(nn.Module):
         x = self.vector_to_tensor(x)
         return self.decoder(x)
 
-def train_decoder3d(decoder, data_loader, device, num_epochs = 5):
+
+def train_decoder3d(decoder, data_loader, device, num_epochs = 3):
     loss_fn = DiceLoss(sigmoid=True)
     optimizer = optim.Adam(decoder.parameters(), lr=1e-3)
-
     # Train decoder
     for epoch in range(num_epochs):
         decoder.train()
         epoch_loss = 0
+
+        iteration_count = 0
         for batch in data_loader:
+            iteration_count += 1
+
             patch_emb = batch["patch"].to(device)
             patch_label = batch["patch_label"].to(device)
-            optimizer.zero_grad()
 
+            optimizer.zero_grad()
             de_output = decoder(patch_emb)
             loss = loss_fn(de_output.squeeze(1), patch_label)
 
@@ -446,7 +450,9 @@ def train_decoder3d(decoder, data_loader, device, num_epochs = 5):
             optimizer.step()
 
             epoch_loss += loss.item()
-        print(f"Epoch {epoch+1}, Loss: {loss / len(data_loader)}")
+
+        print(f"Avg total epoch loss = {epoch_loss / iteration_count:.4f}")
+
 
     return decoder
 
@@ -601,7 +607,6 @@ def inference3d(decoder, data_loader, device, return_binary,  test_cases, test_l
             )
 
             aligned_preds[case_id] = sitk.GetArrayFromImage(pred_on_gt)
-
         return [j for j in aligned_preds.values()]
 
 
@@ -748,7 +753,7 @@ def extract_patch_labels(
     return patch_labels
 
 
-def load_patch_data(data_array: np.ndarray, batch_size: int = 40) -> DataLoader:
+def load_patch_data(data_array: np.ndarray, batch_size: int = 80) -> DataLoader:
     train_ds = dataset_monai(data=data_array)
     train_loader = dataloader_monai(train_ds, batch_size=batch_size, shuffle=False)
     return train_loader
@@ -810,7 +815,7 @@ class SegmentationUpsampling3D(PatchLevelTaskAdaptor):
         test_label_directions,
         patch_size,
         return_binary=True,
-    ):
+    ):   
         label_patch_features = []
         for idx, label in enumerate(shot_labels):
             label_feats = extract_patch_labels(
@@ -862,7 +867,7 @@ class SegmentationUpsampling3D(PatchLevelTaskAdaptor):
             patch_size=self.patch_size,
             labels=self.shot_labels,
         )
-        train_loader = load_patch_data(train_data)
+        train_loader = load_patch_data(train_data, batch_size=10)
         latent_dim = len(self.shot_features[0][0])
         target_patch_size = tuple(int(j / 16) for j in self.patch_size)
         target_shape = (
@@ -904,7 +909,7 @@ class SegmentationUpsampling3D(PatchLevelTaskAdaptor):
             image_directions=self.test_image_directions,
         )
 
-        test_loader = load_patch_data(test_data)
+        test_loader = load_patch_data(test_data, batch_size=20)
         # run inference using the trained decoder
         return inference3d(self.decoder, test_loader, self.device, self.return_binary, self.test_cases, self.test_label_sizes, self.test_label_spacing, self.test_label_origins, self.test_label_directions)
 

src/unicorn_eval/metrics/spider.py
@@ -1,91 +1,8 @@
-import SimpleITK
 from collections import OrderedDict, defaultdict
 import numpy as np
-import SimpleITK as sitk
 import pandas
 
-from scipy.ndimage.morphology import (
-    distance_transform_edt,
-    binary_erosion,
-    generate_binary_structure,
-)
-
-from typing import Iterable, Optional, List, Union
-import json
-from pathlib import Path
-
-
-def surface_distances(
-    manual: Iterable[bool],
-    automatic: Iterable[bool],
-    voxel_spacing: Optional[Iterable[float]] = None,
-    connectivity: Optional[int] = None,
-) -> Iterable[float]:
-    """Computes the surface distances (positive numbers) from all border voxels of a binary object in two images."""
-    manual_mask = np.asarray(manual, dtype="bool")
-    automatic_mask = np.asarray(automatic, dtype="bool")
-
-    if np.count_nonzero(manual_mask) == 0 or np.count_nonzero(automatic_mask) == 0:
-        raise ValueError(
-            "Cannot compute surface distance if there are no foreground pixels in the image"
-        )
-
-    if connectivity is None:
-        connectivity = manual_mask.ndim
-
-    # Extract border using erosion
-    footprint = generate_binary_structure(manual_mask.ndim, connectivity)
-    manual_border = manual_mask ^ binary_erosion(
-        manual_mask, structure=footprint, iterations=1
-    )
-    automatic_border = automatic_mask ^ binary_erosion(
-        automatic_mask, structure=footprint, iterations=1
-    )
-
-    # Compute average surface distance
-    dt = distance_transform_edt(~manual_border, sampling=voxel_spacing)
-    return dt[automatic_border]
-
-
-def average_surface_distance(
-    manual: Iterable[bool],
-    automatic: Iterable[bool],
-    voxel_spacing: Optional[Iterable[float]] = None,
-    connectivity: Optional[int] = None,
-    symmetric: bool = True,
-) -> float:
-    """
-    Computes the average surface distance (ASD) between the binary objects in two images.
-
-    Parameters
-    ----------
-    manual
-        Reference masks (binary)
-
-    automatic
-        Masks that is compared to the reference mask
-
-    voxel_spacing
-        Spacing between elements in the images
-
-    connectivity
-        The neighbourhood/connectivity considered when determining the surface of the binary objects. Values between 1 and ndim are valid.
-        Defaults to ndim, which is full connectivity even along the diagonal.
-
-    symmetric
-        Whether the surface distance are calculated from manual to automatic mask, or symmetrically in both directions
-
-    Returns
-    -------
-    float
-        Average surface distance
-    """
-    sd1 = surface_distances(manual, automatic, voxel_spacing, connectivity)
-    if not symmetric:
-        return np.asarray(sd1).mean()
-
-    sd2 = surface_distances(automatic, manual, voxel_spacing, connectivity)
-    return float(np.concatenate((sd1, sd2)).mean())
+from typing import Iterable
 
 
 def dice_score(mask1: Iterable[bool], mask2: Iterable[bool]) -> float:
@@ -105,20 +22,18 @@ def dice_score(mask1: Iterable[bool], mask2: Iterable[bool]) -> float:
 
 class Spider:
 
-    def __init__(self, gts, preds, spacings, case_ids):
+    def __init__(self, gts, preds, case_ids):
         self.ground_truths = gts
         self.inputs = preds
-        self.spacings = spacings
         self.case_ids = case_ids
 
-    def score_case(self, gt, pred, spacing):
+    def score_case(self, gt, pred):
 
         mask_manual = gt.astype(np.int64)
         mask_automatic = pred.astype(np.int64)
 
         # Construct containers for the per-scan results
         all_dice_scores = defaultdict(list)
-        all_surface_distances = defaultdict(list)
 
         # Check if manual and automatic mask have the same dimensions
         if mask_manual.shape != mask_automatic.shape:
@@ -205,59 +120,21 @@ class Spider:
         detection_rate_vert = (total_vert - missed_vert) / total_vert
         detection_rate_discs = (total_discs - missed_discs) / total_discs
 
-        # Calculate mean absolute surface distances
-        surface_distances_vert = []
-        surface_distances_discs = []
-        for label_manual in all_labels_manual:
-            if label_manual not in label_lut:
-                distance = np.nan
-            else:
-                label_automatic = label_lut[label_manual]
-                distance = average_surface_distance(
-                    mask_manual == label_manual,
-                    mask_automatic == label_automatic,
-                    spacing,
-                )
-
-            if "surface_distance_SC" in locals():
-                pass
-            else:
-                surface_distance_SC = 999
-
-            if label_manual > 0 and label_manual < 100:
-                surface_distances_vert.append(distance)
-            elif label_manual > 200:
-                surface_distances_discs.append(distance)
-            elif label_manual == 100:
-                surface_distance_SC = distance
-            all_surface_distances[label_manual].append(distance)
-
-        surface_distance_vert = np.mean(surface_distances_vert)
-        surface_distance_discs = np.mean(surface_distances_discs)
-        overal_surface_distance = np.mean(
-            [v for vs in all_surface_distances.values() for v in vs]
-        )
-
         return {
             "DiceScoreVertebrae": dice_score_vert,
             "DiceScoreDiscs": dice_score_discs,
             "DiceScoreSpinalCanal": dice_score_SC,
             "OveralDiceScore": overall_dice_score,
             "DetectionRateVertebrae": detection_rate_vert,
-            "DetectionRateDiscs": detection_rate_discs,
-            "ASDVertebrae": surface_distance_vert,
-            "ASDDiscs": surface_distance_discs,
-            "ASDSpinalCanal": surface_distance_SC,
-            "OveralASD": overal_surface_distance,
+            "DetectionRateDiscs": detection_rate_discs
         }
 
     def compute_metrics(self):
-
         metric_accumulator = []
         gts = self.ground_truths
         for i, gt in enumerate(gts):
             metric = self.score_case(
-                gt, self.inputs[i], self.spacings.get(self.case_ids[i])
+                gt, self.inputs[i]
             )
             metric_accumulator.append(metric)
 
@@ -269,10 +146,6 @@ class Spider:
             "OveralDiceScore",
             "DetectionRateVertebrae",
             "DetectionRateDiscs",
-            "ASDVertebrae",
-            "ASDDiscs",
-            "ASDSpinalCanal",
-            "OveralASD",
         ]
 
         results_metric = {}
@@ -285,6 +158,6 @@ class Spider:
         return results_metric.get("OveralDiceScore").get("mean")
 
 
-def compute_spider_score(test_labels, test_predictions, test_image_spacing, case_ids):
-    evaluator = Spider(test_labels, test_predictions, test_image_spacing, case_ids)
+def compute_spider_score(test_labels, test_predictions, case_ids):
+    evaluator = Spider(test_labels, test_predictions, case_ids)
     return evaluator.compute_metrics()