26995234da85ee3db3c8df18fb7cbb3a45682b54
Hartmut Haentze <hartmut.haentze@charite.de>
2025-08-14 17:53:29 +0200

    Use coordinates for patch extraction

--- src/unicorn_eval/adaptors/patch_extraction.py
+++ src/unicorn_eval/adaptors/patch_extraction.py
@@ -1,8 +1,6 @@
 from typing import Iterable
-import numpy as np
 import SimpleITK as sitk
 from picai_prep.preprocessing import resample_img, crop_or_pad
-import itertools
 
 
 def pad_image(image: sitk.Image, patch_size: Iterable[int]) -> sitk.Image:
@@ -34,25 +32,24 @@ def pad_image(image: sitk.Image, patch_size: Iterable[int]) -> sitk.Image:
 
 def extract_patches(
     image: sitk.Image,
+    coordinates: Iterable[tuple[float, float, float]],
     patch_size: Iterable[int],
     spacing: Iterable[float] | None = None,
 ) -> tuple[list[sitk.Image], list[tuple]]:
     """
     Extracts uniformly sized patches from a 3D SimpleITK image, optionally resampling it to a specified voxel spacing before extraction.
-
+    
     If `spacing` is provided, the image is first resampled using linear interpolation to achieve the specified spacing. The image is then padded so that its dimensions become exactly divisible by the given patch size. Patches are extracted systematically, covering the entire image volume without overlap or gaps.
 
     Args:
         image (sitk.Image): Input 3D image from which to extract patches.
+        coordinates (Iterable[tuple[float, float, float]]): Start coordinates for each patch in world coordinates. These are used to determine the physical location of each patch in the original image.
         patch_size (list[int]): Patch size as [x, y, z], defining the dimensions of each extracted patch.
         spacing (list[float] | None, optional): Desired voxel spacing as [x, y, z]. If provided, the image will be resampled to this spacing before patch extraction. Defaults to None.
 
     Returns:
         - patches (list[sitk.Image]): List of extracted image patches.
-        - coordinates (list[tuple]): List of coordinates for each patch, formatted as:
-            ((x_start, y_start, z_start), (x_end, y_end, z_end)) in world coordinates.
-            Each coordinate pair represents the start and end points of the patch in the original image's physical space.
-    """
+     """
     if spacing is not None:
         # resample image to specified spacing
         image = resample_img(
@@ -67,23 +64,13 @@ def extract_patches(
         patch_size=patch_size,
     )
 
-    # generate patch coordinates (x, y, z)
-    image_size = image.GetSize()
-    steps = [range(0, image_size[dim], patch_size[dim]) for dim in range(3)]
-
     patches = []
-    coordinates = []
-    for x, y, z in itertools.product(*steps):
-        start_coords = (x, y, z)
-        patch = sitk.RegionOfInterest(image, patch_size, start_coords)
-        patches.append(patch)
-        matrix_coordinates = (
-            (x, y, z),
-            (x + patch_size[0], y + patch_size[1], z + patch_size[2]),
-        )
-        world_coordinates = tuple(
-            image.TransformIndexToPhysicalPoint(coord) for coord in matrix_coordinates
-        )
-        coordinates.append(world_coordinates)
+    for coord in coordinates:
+        # Convert start coordinate from world space to index space
+        start_index = image.TransformPhysicalPointToIndex(coord)
 
-    return patches, coordinates
+        # Extract ROI
+        patch = sitk.RegionOfInterest(image, patch_size, start_index)
+        patches.append(patch)
+
+    return patches
