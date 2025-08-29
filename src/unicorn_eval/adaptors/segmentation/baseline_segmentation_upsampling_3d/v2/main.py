658cb0d7fd9f76e02104ef88f45d63798fa22af2
Joeran Bosma <joeran@bosma.co>
2025-08-29 08:50:37 +0200

    Copy current version to highlight changes

---
+++ src/unicorn_eval/adaptors/segmentation/baseline_segmentation_upsampling_3d/v2/main.py
@@ -0,0 +1,202 @@
+#  Copyright 2025 Diagnostic Image Analysis Group, Radboudumc, Nijmegen, The Netherlands
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import annotations
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+from unicorn_eval.adaptors.base import PatchLevelTaskAdaptor
+from unicorn_eval.adaptors.segmentation.data_handling import (
+    construct_data_with_labels, extract_patch_labels, load_patch_data)
+from unicorn_eval.adaptors.segmentation.decoders import Decoder3D
+from unicorn_eval.adaptors.segmentation.inference import inference3d
+from unicorn_eval.adaptors.segmentation.training import train_decoder3d
+
+
+class SegmentationUpsampling3D(PatchLevelTaskAdaptor):
+    """
+    Patch-level adaptor that trains a 3D upsampling decoder for segmentation.
+
+    This adaptor takes precomputed patch-level features from 3D medical images
+    and performs segmentation by training a decoder that upsamples the features
+    back to voxel space.
+
+    Steps:
+    1. Extract patch-level segmentation labels using spatial metadata.
+    2. Construct training data from patch features and coordinates.
+    3. Train a 3D upsampling decoder to predict voxel-wise segmentation from patch embeddings.
+    4. At inference, apply the trained decoder to test patch features and reconstruct full-size predictions.
+
+    Args:
+        shot_features : Patch-level feature embeddings of few shots used for for training.
+        shot_labels : Full-resolution segmentation labels.
+        shot_coordinates : Patch coordinates corresponding to shot_features.
+        shot_names : Case identifiers for few shot patches.
+        test_features : Patch-level feature embeddings for testing.
+        test_coordinates : Patch coordinates corresponding to test_features.
+        test_names : Case identifiers for testing patches.
+        test_image_sizes, test_image_origins, test_image_spacings, test_image_directions:
+            Metadata for reconstructing full-size test predictions.
+        shot_image_spacing, shot_image_origins, shot_image_directions:
+            Metadata for extracting training labels at patch-level.
+        patch_size : Size of each 3D patch.
+        return_binary : Whether to threshold predictions to binary masks.
+        balance_bg : Whether to balance background and foreground patches using inverse probability weighting.
+    """
+
+    def __init__(
+        self,
+        shot_features,
+        shot_coordinates,
+        shot_names,
+        shot_labels,
+        shot_image_spacing,
+        shot_image_origins,
+        shot_image_directions,
+        shot_image_sizes,
+        shot_label_spacing,
+        shot_label_origins,
+        shot_label_directions,
+        test_features,
+        test_coordinates,
+        test_names,
+        test_image_sizes,
+        test_image_origins,
+        test_image_spacings,
+        test_image_directions,
+        test_label_sizes,
+        test_label_spacing,
+        test_label_origins,
+        test_label_directions,
+        patch_size,
+        patch_spacing,
+        return_binary=True,
+        balance_bg=False,
+    ):
+        label_patch_features = []
+        for idx, label in tqdm(enumerate(shot_labels), desc="Extracting patch labels"):
+            label_feats = extract_patch_labels(
+                label=label,
+                label_spacing=shot_label_spacing[shot_names[idx]],
+                label_origin=shot_label_origins[shot_names[idx]],
+                label_direction=shot_label_directions[shot_names[idx]],
+                image_size=shot_image_sizes[shot_names[idx]],
+                image_origin=shot_image_origins[shot_names[idx]],
+                image_spacing=shot_image_spacing[shot_names[idx]],
+                image_direction=shot_image_directions[shot_names[idx]],
+                start_coordinates=shot_coordinates[idx],
+                patch_size=patch_size,
+                patch_spacing=patch_spacing,
+            )
+            label_patch_features.append(label_feats)
+        label_patch_features = np.array(label_patch_features, dtype=object)
+
+        super().__init__(
+            shot_features=shot_features,
+            shot_labels=label_patch_features,
+            shot_coordinates=shot_coordinates,
+            test_features=test_features,
+            test_coordinates=test_coordinates,
+            shot_extra_labels=None,  # not used here
+        )
+
+        self.shot_names = shot_names
+        self.test_cases = test_names
+        self.test_image_sizes = test_image_sizes
+        self.test_image_origins = test_image_origins
+        self.test_image_spacings = test_image_spacings
+        self.test_image_directions = test_image_directions
+        self.shot_image_spacing = shot_image_spacing
+        self.shot_image_origins = shot_image_origins
+        self.shot_image_directions = shot_image_directions
+        self.test_label_sizes = test_label_sizes
+        self.test_label_spacing = test_label_spacing
+        self.test_label_origins = test_label_origins
+        self.test_label_directions = test_label_directions
+        self.patch_size = patch_size
+        self.patch_spacing = patch_spacing
+        self.decoder = None
+        self.return_binary = return_binary
+        self.balance_bg = balance_bg
+
+    def fit(self):
+        # build training data and loader
+        train_data = construct_data_with_labels(
+            coordinates=self.shot_coordinates,
+            embeddings=self.shot_features,
+            cases=self.shot_names,
+            patch_size=self.patch_size,
+            patch_spacing=self.patch_spacing,
+            labels=self.shot_labels,
+        )
+
+        train_loader = load_patch_data(train_data, batch_size=10, balance_bg=self.balance_bg)
+        latent_dim = len(self.shot_features[0][0])
+        target_patch_size = tuple(int(j / 16) for j in self.patch_size)
+        target_shape = (
+            latent_dim,
+            target_patch_size[2],
+            target_patch_size[1],
+            target_patch_size[0],
+        )
+
+        # set up device and model
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        decoder = Decoder3D(
+            latent_dim=latent_dim,
+            target_shape=target_shape,
+            decoder_kwargs={
+                "spatial_dims": 3,
+                "init_filters": 32,
+                "latent_channels": latent_dim,
+                "out_channels": 1,
+                "blocks_up": (1, 1, 1, 1),
+                "dsdepth": 1,
+                "upsample_mode": "deconv",
+            },
+        )
+
+        decoder.to(self.device)
+        self.decoder = train_decoder3d(decoder, train_loader, self.device)
+
+    def predict(self) -> list:
+        # build test data and loader
+        test_data = construct_data_with_labels(
+            coordinates=self.test_coordinates,
+            embeddings=self.test_features,
+            cases=self.test_cases,
+            patch_size=self.patch_size,
+            patch_spacing=self.patch_spacing,
+            image_sizes=self.test_image_sizes,
+            image_origins=self.test_image_origins,
+            image_spacings=self.test_image_spacings,
+            image_directions=self.test_image_directions,
+        )
+
+        test_loader = load_patch_data(test_data, batch_size=10)
+
+        # run inference using the trained decoder
+        return inference3d(
+            decoder=self.decoder,
+            data_loader=test_loader,
+            device=self.device,
+            return_binary=self.return_binary,
+            test_cases=self.test_cases,
+            test_label_sizes=self.test_label_sizes,
+            test_label_spacing=self.test_label_spacing,
+            test_label_origins=self.test_label_origins,
+            test_label_directions=self.test_label_directions
+        )
