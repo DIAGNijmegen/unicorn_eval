e492bddb7c45d1a2c45c3e1abef965e224a929ea
clemsgrs <clementnet@outlook.com>
2025-09-17 08:07:18 +0000

    remove multiprocessing

--- src/unicorn_eval/evaluate.py
+++ src/unicorn_eval/evaluate.py
@@ -354,7 +354,6 @@ def process_task_in_subprocess(
     logging.info(f"Processing task in subprocess: {task_name}")
 
     modality = mapping[(mapping.task_name == task_name)]["modality"].values[0]
-    max_workers = get_max_workers()
 
     if modality == "vision":
 
@@ -383,13 +382,10 @@ def process_task_in_subprocess(
                 input_dir=INPUT_DIRECTORY, case_names=task_shots
             )
 
-            pool = multiprocessing.Pool(processes=max_workers)
-            shots = pool.map(process, shot_inputs)
-            pool.close()
-            pool.join()
-
-            shot_informations = extract_embeddings_and_labels(shots, task_name)
+            shots = process(shot_inputs)
             del shot_inputs
+            gc.collect()
+            shot_informations = extract_embeddings_and_labels(shots, task_name)
             del shots
             gc.collect()
 
@@ -460,6 +456,7 @@ def process_task_in_subprocess(
                     shot_labels,
                     shot_extra_labels,
                     shot_ids,
+                    shot_coordinates,
                     shot_image_sizes,
                     shot_image_spacings,
                     shot_image_origins,
@@ -477,13 +474,10 @@ def process_task_in_subprocess(
                 case_inputs = read_inputs(
                     input_dir=INPUT_DIRECTORY, case_names=task_cases
                 )
-                pool = multiprocessing.Pool(processes=max_workers)
-                cases = pool.map(process, case_inputs)
-                pool.close()
-                pool.join()
-
-                case_information = extract_labels(cases, task_name)
+                cases = process(case_inputs)
                 del case_inputs
+                gc.collect()
+                case_information = extract_labels(cases, task_name)
                 del cases
                 gc.collect()
 
@@ -530,11 +524,7 @@ def process_task_in_subprocess(
         case_inputs = read_inputs(
             input_dir=INPUT_DIRECTORY, case_names=task_shots
         )
-        pool = multiprocessing.Pool(processes=max_workers)
-        cases = pool.map(process, case_inputs)
-        pool.close()
-        pool.join()
-
+        cases = process(case_inputs)
         case_information = extract_embeddings_and_labels(cases, task_name)
         if case_information is None:
             raise ValueError(f"No cases found for task {task_name}")
