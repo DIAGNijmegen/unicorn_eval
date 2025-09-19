4e1026e29b038daae1e9abd8ff50991cc7d78581
clemsgrs <clementnet@outlook.com>
2025-09-19 08:45:47 +0000

    put back multiprocessing when loading algorithm results

--- src/unicorn_eval/evaluate.py
+++ src/unicorn_eval/evaluate.py
@@ -354,6 +354,7 @@ def process_task_in_subprocess(
     logging.info(f"Processing task in subprocess: {task_name}")
 
     modality = mapping[(mapping.task_name == task_name)]["modality"].values[0]
+    max_workers = get_max_workers()
 
     if modality == "vision":
 
@@ -382,7 +383,11 @@ def process_task_in_subprocess(
                 input_dir=INPUT_DIRECTORY, case_names=task_shots
             )
 
-            shots = [process(shot_input) for shot_input in shot_inputs]
+            pool = multiprocessing.Pool(processes=max_workers)
+            shots = pool.map(process, shot_inputs)
+            pool.close()
+            pool.join()
+            # shots = [process(shot_input) for shot_input in shot_inputs]
             del shot_inputs
             gc.collect()
             shot_informations = extract_embeddings_and_labels(shots, task_name)
@@ -474,7 +479,11 @@ def process_task_in_subprocess(
                 case_inputs = read_inputs(
                     input_dir=INPUT_DIRECTORY, case_names=task_cases
                 )
-                cases = [process(case_input) for case_input in case_inputs]
+                pool = multiprocessing.Pool(processes=max_workers)
+                cases = pool.map(process, case_inputs)
+                pool.close()
+                pool.join()
+                # cases = [process(case_input) for case_input in case_inputs]
                 del case_inputs
                 gc.collect()
                 case_information = extract_labels(cases, task_name)
